0b7eaa2e2e2837186dd8c549047f67c8120ed394
TonyDawson <tonydawson211@gmail.com>
2025-11-08 15:18:10 +0800

    second commit

--- src/main.py
+++ src/main.py
@@ -2,5 +2,5 @@ from src.CNN_Test import Test
 from src.CNN_Model_Device import model
 from src.CNN_Training_Data_Load import test_loader
 
-if __name__ == "main":
+if __name__ == "__main__":
     te_acc_avr, best_tr_acc_avr, best_tr_state, tl, ta, vl, va = Test(model, test_loader)
